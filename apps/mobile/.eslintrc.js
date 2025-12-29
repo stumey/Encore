e2e30152086464b931fbd458c6115711c5e37147
stumey <stefan.tumey@gmail.com>
2025-12-28 20:07:54 -0500

    eslient for mobile

---
+++ apps/mobile/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['expo', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
